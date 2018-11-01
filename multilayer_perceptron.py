eeb0ece146f548ac5573699190a4b3846310c796
Ga0aG <708835987@qq.com>
2018-11-01 09:48:07 +0800

    Update multilayer_perceptron.py

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -1,15 +1,8 @@
-
-
 from __future__ import print_function
-
 import tensorflow as tf
 
-# Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
-#import mnitst
-#mnist=mnitst.read_data_sets("/tmp/data/", one_hot=True)
 
 # Parameters
 learning_rate = 0.01
